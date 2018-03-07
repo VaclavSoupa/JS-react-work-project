41b5504b5d57b744b916f7485b15dfff995e008e
Václav Šoupa <vaclav.soupa@gmail.com>
2018-03-07 17:42:11 +0100

    6.0

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 //library imports
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const humanDefinition = {
+  name: "",
+  surname: ""
+};
 
 class App extends Component {
-  state={
-    lidi:
-    [{name:"Václav",Surname:"Šoupa"},
-    {name:"Pepa",Surname:"Altman"},
-    {name:"Tomáš",Surname:"Fejtek"},
-    {name:"Lukáš",Surname:"Gryč"}
-  ],
-  value:""
-  }
+  state = {
+    lidi: [
+      { name: "Václav", surname: "Šoupa" },
+      { name: "Pepa", surname: "Altman" },
+      { name: "Tomáš", surname: "Fejtek" },
+      { name: "Lukáš", surname: "Gryč" }
+    ],
+    searchValue: ""
+  };
 
   handleCreateRow = () => {
-      const {lidi, value} = this.state
-      this.setState({
-          value: "",
-          lidi:[
-              ...lidi,
-              {
-                  name: value,
-              }
-          ]
-      });
+    const { lidi } = this.state;
+    this.setState({
+      lidi: [...lidi, { ...humanDefinition }]
+    });
   };
-/*  handleCreateCol = ()
+  /*  handleCreateCol = ()
   => {
   const {lidi} = this.state
     this.setState({
@@ -35,54 +34,91 @@ class App extends Component {
       ]
     })
   }*/
-  handleChange=(event)=>{
-    this.setState({value: event.target.value})
-  }
-  handleChangeLidi = (event ,index) => {
-      const {lidi} = this.state
-      this.setState({
-          lidi:[
-              ...lidi.slice(0, index),
-              {
-                  ...lidi[index],
-                  name:event.target.value
-              },
-              ...lidi.slice(index+1)
-          ]
-      });
-  };
-  render() {
-    const {lidi,value} = this.state
-    const filterPeople = lidi.filter(value=>value.name.includes(this.state.value)||
-                                           value.Surname.includes(this.state.value))
-    return(
 
-<div>
-<form className='App'>
-<input type="Button" value="ADD" onClick={this.handleCreateRow}/>
-{/*<input type="Button" value="ADD" onClick={this.handleCreateCol}/>*/}
-<input type="text" value={this.state.value} onChange={this.handleChange}/>
-<table className="App-table">
-<th>Name</th>
-<th>Surname</th>
-  {filterPeople.map((human, index)=>
-    <tr>
-    <td>
-    <input type="text" value={human.name} onChange={(event)=>
-                              this.handleChangeLidi(event, index)
-                            }/>
-                              </td>
-                        <td>
-              <input type="textField" value={human.Surname}
-         onChange={(event)=>this.handleChangeLidi(event, index)}/>
-         </td>
-    </tr>
-  )
-  }
-</table>
-</form>
-</div>
-   );
+  handleChangeSearch = event => {
+    this.setState({ searchValue: event.target.value });
+  };
+
+  handleChangeLidi = (event, index, type) => {
+    const { lidi } = this.state;
+    this.setState({
+      lidi: [
+        ...lidi.slice(0, index),
+        {
+          ...lidi[index],
+          [type]: event.target.value
+        },
+        ...lidi.slice(index + 1)
+      ]
+    });
+  };
+
+  render() {
+    const { lidi, searchValue } = this.state;
+    const filterPeople = lidi.filter(
+      value =>
+        value.name.includes(searchValue) || value.surname.includes(searchValue)
+    );
+
+    return (
+      <div>
+        <form className="App">
+          {/*<input type="Button" value="ADD" onClick={this.handleCreateCol}/>*/}
+
+          <div class="input-group">
+            <span class="input-group-addon">Search:</span>
+            <input
+              className="form-control"
+              type="text"
+              value={this.state.searchValue}
+              onChange={event => this.handleChangeSearch(event)}
+            />
+            <span class="input-group-btn">
+              <button
+                className="btn btn-secondary"
+                type="button"
+                value="ADD"
+                onClick={this.handleCreateRow}
+              >
+                Add
+              </button>
+            </span>
+          </div>
+          <table className="table table-striped">
+            <thead>
+              <th>Name</th>
+              <th>Surname</th>
+            </thead>
+            <tbody>
+              {filterPeople.map((human, index) => (
+                <tr>
+                  <td>
+                    <input
+                      className="form-control"
+                      type="text"
+                      value={human.name}
+                      onChange={event =>
+                        this.handleChangeLidi(event, index, "name")
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="form-control"
+                      type="textField"
+                      value={human.surname}
+                      onChange={event =>
+                        this.handleChangeLidi(event, index, "surname")
+                      }
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </form>
+      </div>
+    );
   }
 }
 
