1ca417f089fde2a3ffcac17097060648d2b91303
Václav Šoupa <Vaclav.Soupa@KornFerry.com>
2018-03-07 12:37:15 +0100

    4.0

--- src/App.js
+++ src/App.js
@@ -12,25 +12,76 @@ class App extends Component {
   ],
   value:""
   }
+
+  handleCreateRow = () => {
+      const {lidi, value} = this.state
+      this.setState({
+          value: "",
+          lidi:[
+              ...lidi,
+              {
+                  name: value,
+              }
+          ]
+      });
+  };
+/*  handleCreateCol = ()
+  => {
+  const {lidi} = this.state
+    this.setState({
+      value: "",
+      lidi:[
+
+      ]
+    })
+  }*/
   handleChange=(event)=>{
     this.setState({value: event.target.value})
   }
+  handleChangeLidi = (event ,index) => {
+      const {lidi} = this.state
+      this.setState({
+          lidi:[
+              ...lidi.slice(0, index),
+              {
+                  ...lidi[index],
+                  name:event.target.value
+              },
+              ...lidi.slice(index+1)
+          ]
+      });
+  };
   render() {
     const {lidi,value} = this.state
     const filterPeople = lidi.filter(value=>value.name.includes(this.state.value)||
                                            value.Surname.includes(this.state.value))
     return(
+
+<div>
 <form className='App'>
-<input type="text" value={this.state.value} onChange={this.handleChange} />
-<table className="App-table"><th>Name</th><th>Surname</th>
-  {filterPeople.map((human)=>
-    <tr><td>{human.name}</td><td>{human.Surname}</td></tr>
+<input type="Button" value="ADD" onClick={this.handleCreateRow}/>
+{/*<input type="Button" value="ADD" onClick={this.handleCreateCol}/>*/}
+<input type="text" value={this.state.value} onChange={this.handleChange}/>
+<table className="App-table">
+<th>Name</th>
+<th>Surname</th>
+  {filterPeople.map((human, index)=>
+    <tr>
+    <td>
+    <input type="text" value={human.name} onChange={(event)=>
+                              this.handleChangeLidi(event, index)
+                            }/>
+                              </td>
+                        <td>
+              <input type="textField" value={human.Surname}
+         onChange={(event)=>this.handleChangeLidi(event, index)}/>
+         </td>
+    </tr>
   )
   }
-
 </table>
 </form>
-
+</div>
    );
   }
 }
