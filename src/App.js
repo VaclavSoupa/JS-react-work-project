b25b8b3a106dc6b33cf391b1c3356da85dd7bd8d
Václav Šoupa <Vaclav.Soupa@KornFerry.com>
2018-03-16 13:57:51 +0100

    Modal fuction

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Modal from "react-modal";
 import "./App.css";
 
+//Modal CSS
 const customStyles = {
   content: {
     top: "50%",
@@ -13,12 +14,14 @@ const customStyles = {
     transform: "translate(-50%, -50%)"
   }
 };
+//Creating parametrs
 const humanDefinition = {
   name: "",
   surname: ""
 };
-
+//Class extending Component
 class App extends Component {
+  //creating state
   state = {
     lidi: [
       { name: "Václav", surname: "Šoupa" },
@@ -27,36 +30,39 @@ class App extends Component {
       { name: "Lukáš", surname: "Gryč" }
     ],
     searchValue: "",
-    isShowingModal: false
+    isShowingModal: false,
+    parametrName: ""
   };
-
+  //Modal function on/off
   handleClick = () => this.setState({ isShowingModal: true });
   handleClose = () => this.setState({ isShowingModal: false });
+  //Button ADD row function
   handleCreateRow = () => {
     const { lidi } = this.state;
     this.setState({
       lidi: [...lidi, { ...humanDefinition }]
     });
   };
+  //Button ADD COL function
   handleCreateCol(event) {
     var newArray = this.state.arr.slice();
-    newArray.push("new value");
+    newArray.push(event.target.value);
     this.setState({ arr: newArray });
   }
-
+  handleChange = event =>{
+    this.setState({parametrName: event.target.value})
+  }
+  //Search function
   handleChangeSearch = event => {
     this.setState({ searchValue: event.target.value });
   };
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = "#f00";
-  }
+  //
   handleAddLidi = (event, index, type) => {
     const { lidi } = this.state;
     this.setState({
       lidi: [
         ...lidi.slice(0, index),
-        {
+       {
           ...lidi[index],
           [type]: event.target.value
         },
@@ -64,46 +70,55 @@ class App extends Component {
       ]
     });
   };
-
+  //Remove buttons function
   handleRemoveLidi(index) {
     const lidi = this.state.lidi;
     lidi.splice(index, 1);
     this.setState({ lidi });
   }
-
+  //Render function all output is here
   render() {
-    const { lidi, searchValue } = this.state;
+    const { lidi, searchValue, parametrName } = this.state;
     const filterPeople = lidi.filter(
       value =>
         value.name.includes(searchValue) || value.surname.includes(searchValue)
     );
-
+    // HTML in return of render function
     return (
       <div>
         <div>
           <Modal
             isOpen={this.state.isShowingModal}
-            onRequestClose={this.closeModal}
             style={customStyles}
             shouldCloseOnOverlayClick={true}
             onRequestClose={this.handleClose}
-            contentLabel="Example Modal"
-            class="modal-dialog"
-          ><div class="modal-header">
-          <h4 class="modal-title">Parametr name:</h4>
-            <button type="button" className="close" onClick={this.handleClose}>&times;</button>
+
+          >
+            <div class="modal-header">
+              <h4 class="modal-title">Parametr name:</h4>
+              <button
+                type="button"
+                className="close"
+                onClick={this.handleClose}
+
+              >
+                &times;
+              </button>
             </div>
             <form className="form-inline">
               <div>
                 <input
                   className="form-control mb-2 mr-sm-2 mb-sm-0"
                   type="text"
+                  value={this.state.parametrName}
+                  onChange={event => this.handleChange(event)}
                 />
                 <button
                   className="btn btn-primary"
                   type="button"
-                  value="ADD"
-                  onClick={this.isShowingModal}
+                  value={this.state.parametrName}
+                  onClick={this.handleCreateCol, this.handleClose}
+
                 >
                   Add
                 </button>
